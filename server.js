06583ae6ee010ed95103021f28879cb5c4896c7c
bk23 <bk23@localhost.localdomain>
2020-07-23 15:50:22 +0300

    added a basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+const net = require('net');
+
+const server = net.createServer(connection => {
+	console.log(`New Client ${connection.remoteAddress}`);
+
+	connection.on('data', data => connection.write(`Hello, ${data}`));
+	connection.on('end', () => console.log('Client Left'));
+});
+
+server.listen(9000);
